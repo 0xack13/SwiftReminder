f0f6ca1682cc8beb7686ac495f14fc82bbc01ffd
Saleh Albatati <0xack13@gmail.com>
2014-10-25 03:14:53 +0300

    Initial Commit

---
+++ SwiftReminderTests/SwiftReminderTests.m
@@ -0,0 +1,34 @@
+//
+//  SwiftReminderTests.m
+//  SwiftReminderTests
+//
+//  Created by 0xack13 on 10/25/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SwiftReminderTests : XCTestCase
+
+@end
+
+@implementation SwiftReminderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
