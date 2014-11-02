673236bc93b1a9da2ea463dc3603f4c439ac187d
Saleh Albatati <0xack13@gmail.com>
2014-11-03 01:31:55 +0300

    Dynamic random messages based on the maximum number in the array - array count

--- SwiftReminder/ViewController.m
+++ SwiftReminder/ViewController.m
@@ -28,15 +28,15 @@
 }
 
 - (IBAction)buttonPressed:(id)sender {
-    int fromNumber = 0;
-    int toNumber = 2;
-    int randomNumber = (arc4random()%(toNumber-fromNumber))+fromNumber;
-    
-    
     NSString * f = @"اللهم صلِّ وسلم وبارك على سيدنا محمد وعلى آل سيدنا محمد قطب السيادة والمجد والفخار ، وخير من تهجد لربه آناء الليل وأطراف النهار .";
     NSString * b = @"اللهم صلِّ وسلم وبارك على سيدنا محمد وعلى آل سيدنا محمد عين سر الوجود بلسان كل معدوم وموجود ، من حباه الله وأكرمه بمنةٍ وعطاءٍ وفضلٍ وجودٍ .";
     NSString * z = @"اللهم صلِّ وسلم وبارك على سيدنامحمد وعلى آل سيدنا محمد خير من أقتبست منه بدور الكائنات ضياءها ، وبه سائر المخلوقات لم تقطع من الله يوم القيامة رجاءها .";
-    NSArray  * myArray2 = [NSArray arrayWithObjects:f,b,z,nil];
+    NSArray  * myArray2 = [NSArray arrayWithObjects:f,b,z, NULL];
+    NSLog(@"Number of Elements in the Array is: %d", [myArray2 count]);
+    
+    int fromNumber = 0;
+    int toNumber = [myArray2 count] - 1;
+    int randomNumber = (arc4random()%(toNumber-fromNumber))+fromNumber;
     
     NSLog(@"The element at index %d in the array is: %@", randomNumber, [myArray2 objectAtIndex:randomNumber]);
     
